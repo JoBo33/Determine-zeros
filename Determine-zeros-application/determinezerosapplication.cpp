56d606d00c48bdc8385734c704315600046ded49
Bosse <joelbosse@gmx.de>
2021-11-05 11:34:49 +0100

    Create regula falsi method with example functions

--- Determine-zeros-application/determinezerosapplication.cpp
+++ Determine-zeros-application/determinezerosapplication.cpp
@@ -40,30 +40,30 @@ void DetermineZerosApplication::bisection(){
     while((int)(oldBoundaryX*1000) != (int)(newBoundaryX*1000));
 }
 void DetermineZerosApplication::regulaFalsi(){
- //   // example function f(x) = x³-2x+2
- //   double leftBoundaryX = -2;
- //   double rightBoundaryX = 0;
- //   double leftBoundaryF = qPow(leftBoundaryX,3)-2*leftBoundaryX + 2; //function(leftBoundaryX);
- //   double rightBoundaryF = qPow(rightBoundaryX,3)-2*rightBoundaryX + 2; //function(rightBoundaryX);
- //   double newBoundaryX; //= (leftBoundaryX*leftBoundaryF - rightBoundaryX*rightBoundaryF)/(rightBoundaryF-leftBoundaryF);
- //   double newBoundaryF;
- //   double oldBoundaryX;
- //   double oldBoundaryF;
- //   do{
- //       oldBoundaryX = newBoundaryX;
- //       oldBoundaryF = newBoundaryF;
- //       newBoundaryX = (leftBoundaryX*leftBoundaryF - rightBoundaryX*rightBoundaryF)/(rightBoundaryF-leftBoundaryF);
- //       newBoundaryF = qPow(newBoundaryX,3)-2*newBoundaryX + 2; //function(newBoundaryX);
- //       if((newBoundaryF < 0 && leftBoundaryF < 0) || (newBoundaryF > 0 && leftBoundaryF > 0)){
- //           leftBoundaryX = newBoundaryX;
- //           leftBoundaryF = newBoundaryF;
- //       }
- //       else if((newBoundaryF < 0 && rightBoundaryF < 0) || (newBoundaryF > 0 && rightBoundaryF > 0)){
- //           rightBoundaryX = newBoundaryX;
- //           rightBoundaryF = newBoundaryF;
- //       }
- //   }
- //   while((int)oldBoundaryX*1000 != (int)newBoundaryX*1000);
+    // example function f(x) = x³-2x+2
+    double leftBoundaryX = -2;
+    double rightBoundaryX = 0;
+    double leftBoundaryF = qPow(leftBoundaryX,3)-2*leftBoundaryX + 2; //function(leftBoundaryX);
+    double rightBoundaryF = qPow(rightBoundaryX,3)-2*rightBoundaryX + 2; //function(rightBoundaryX);
+    double newBoundaryX; //= (leftBoundaryX*leftBoundaryF - rightBoundaryX*rightBoundaryF)/(rightBoundaryF-leftBoundaryF);
+    double newBoundaryF;
+    double oldBoundaryX;
+    double oldBoundaryF;
+    do{
+        oldBoundaryX = newBoundaryX;
+        oldBoundaryF = newBoundaryF;
+        newBoundaryX = (leftBoundaryX*rightBoundaryF - rightBoundaryX*leftBoundaryF)/(rightBoundaryF-leftBoundaryF);
+        newBoundaryF = qPow(newBoundaryX,3)-2*newBoundaryX + 2; //function(newBoundaryX);
+        if((newBoundaryF < 0 && leftBoundaryF < 0) || (newBoundaryF > 0 && leftBoundaryF > 0)){
+            leftBoundaryX = newBoundaryX;
+            leftBoundaryF = newBoundaryF;
+        }
+        else if((newBoundaryF < 0 && rightBoundaryF < 0) || (newBoundaryF > 0 && rightBoundaryF > 0)){
+            rightBoundaryX = newBoundaryX;
+            rightBoundaryF = newBoundaryF;
+        }
+    }
+    while((int)(oldBoundaryX*1000) != (int)(newBoundaryX*1000));
 }
 void DetermineZerosApplication::newtonRaphson(){
     // example function f(x) = x³+3x²-x-8
